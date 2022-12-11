593aaac4c58e436caaa244b6337e94fa8660ae9e
unknown <jonas.ostenstad@student.uib.no>
2022-12-12 00:53:00 +0100

    Solution for day 11 part 1

--- day11/day11.py
+++ day11/day11.py
@@ -5,9 +5,39 @@ data = sys.stdin.read().split('\n\n')
 
 def monkey_parse(data):
     data = data.split('\n')
-    print(data)
     start_items = data[1].split(':')[1].split(',')
-    operation = data[2][-4]
-    return data
+    start_items = [int(n) for n in start_items]
+    operation = data[2].split('=')[-1].strip()
+    test_factor = int(data[3].split(' ')[-1])
+    true = int(data[4].split(' ')[-1])
+    false = int(data[5].split(' ')[-1])
 
-monkey_parse(data[0])
+    return [start_items, operation, test_factor, true, false, 0]
+
+def monkey_throw(monkey):
+    start_items, operation, test_factor, true, false, inspected = monkey
+    while start_items:
+        old = start_items.pop(0)
+        item = eval(operation) // 3
+        if(item % test_factor == 0):
+            monkeys[true][0].append(item)
+        else:
+            monkeys[false][0].append(item)
+        monkey[-1] += 1
+            
+        
+def sol1(monkeys):
+    n = 20
+    for _ in range(n):
+        for monkey in monkeys:
+            monkey_throw(monkey)
+
+    inspect = []
+    for monkey in monkeys:
+        inspect.append(monkey[-1])
+
+    a, b = sorted(inspect, reverse=True)[:2]
+    return a * b
+
+monkeys = [monkey_parse(e) for e in data]
+print(sol1(monkeys))

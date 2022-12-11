04521e5da61e7bc2a259980f52c69a03414ec7c2
unknown <jonas.ostenstad@student.uib.no>
2022-12-11 17:54:09 +0100

    day 9 works for test case, but not actual input, debugging is needed

--- day9/day9.py
+++ day9/day9.py
@@ -1,48 +1,57 @@
 import sys
 
-data = sys.stdin.readlines()
-data = [[e[0], int(e.split()[1])] for e in data]
-
-x, y = 0, 0
-i, j = 0, 0
-
-tail_log = []
-
-def neighbour(i,j,x,y):
+def neighbour(a, b):
+    x,y = a
+    i,j = b
     for a in range(-1, 2):
        for b in range(-1, 2):
-            if((i+a,j+b) == (x,y)):
+            if (i+a,j+b) == (x,y):
                 return True
     
     return False
 
-def move():
-    for a in range(-1, 2):
-       for b in range(-1, 2):
-        if(neighbour(i+a,j+b, x,y)):
-            i = i+a
-            j = j+b
+def move(a, b):
+    x,y = a
+    i,j = b
+    for c in range(-1, 2):
+       for d in range(-1, 2):
+        if(neighbour([i+c, j+d], b)):
+            return( [i+c, j+d] )
+    
+def move_head():
+    global pos, tail_pos
+    if(not neighbour(tail_pos, pos)):
+        tail_pos = move(tail_pos, pos)
 
+    tail_log.append(tail_pos)
 
+data = sys.stdin.readlines()
+data = [[e[0], int(e.split()[1])] for e in data]
+
+pos = [0,0]
+tail_pos = [0,0]
+
+tail_log = []
 
 for line in data:
-    templog = []
     if line[0] == 'R':
         for _ in range(line[1]):
-            x += 1
+            pos[0] += 1
+            move_head()
+    
     if line[0] == 'L':
         for _ in range(line[1]):
-            x -= 1
+            pos[0] -= 1
+            move_head()
+
     if line[0] == 'U':
         for _ in range(line[1]):
-            y += 1
+            pos[1] += 1     
+            move_head()
+
     if line[0] == 'D':
         for _ in range(line[1]):
-            y -= 1
+            pos[1] -= 1
+            move_head()
     
-    if (neighbour(i,j, x,y)):
-        continue
-    else:
-        move()
-        
-    tail_log.append((i,j))
+print(len(set([tuple(i) for i in tail_log])))
